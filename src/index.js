c74266a16f2a07837af0617989c9fbf8dbbdeb44
dehuihu <dehuihu@gmail.com>
2025-05-20 11:28:17 -0700

    Co-authored-by: Jamie Hendrickson <johendrickson@users.noreply.github.com> Co-authored-by: Lina Martinez <linakl19@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-function divides(num1, num2) {
-  return num1 % num2 === 0;
-}
+// function divides(num1, num2) {
+//   return num1 % num2 === 0;
+// }
 
 // Convert the above function to arrow function
-
-
+// const divides = (num1, num2) => num1 % num2 === 0;
+// console.log(divides(20, 5));
 
 // Write a function that converts Fahrenheit to Celsius
-
-
+// const fahrenheitToCelsius = (fahrenheit) => {
+//   return (fahrenheit - 32) * 5 / 9;
+// };
+// console.log(fahrenheitToCelsius(100));
 
 
 // Convert the python function below to a javascript function
@@ -20,7 +22,19 @@ function divides(num1, num2) {
 //             if numbers[i]+numbers[j] == target:
 //                 total += 1
 //     return total
+const pairsWithGivenSum = (numbers, target) => {
+  let total = 0;
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++){
+      if (numbers[i] + numbers[j] === target){
+        total += 1;
+      }
+    }
+  }
+  return total;
+};
 
+console.log(pairsWithGivenSum([1, 3, 4, 6], 7));
 
 // Using the setInterval function (described below), register a callback function to be called every second.
 // Consider printing some output so that you can see that the callback is running.
@@ -33,3 +47,8 @@ function divides(num1, num2) {
 // Check out the MDN documentation for setInterval (and setTimeout) for additional details
 
 // add your call here
+
+
+setInterval(() => {
+  console.log('This prints every second!');
+}, 1000);
